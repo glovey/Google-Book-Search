31a33a5ee56016153fc9dc2f562f6a6e3162c724
glovey <jmfilby@gmail.com>
2022-12-19 23:02:19 +0000

    Improved commentary and added Docstrings

--- add_to_reading_list.py
+++ add_to_reading_list.py
@@ -1,28 +1,40 @@
 from print_library import print_library
 
-def add_to_reading_list(query_results, reading_list):
-  add_book = None
-  while add_book not in ["yes", "no"]:
-    add_book = input("Would you like to add a book to you reading list? Answer yes or no.\n").lower()
-    if add_book not in ["yes", "no"]:
-      print ("that's not a valid option, please try again\n")
 
-  
-  while add_book == "yes":
-    book_choice = int(input(f"which book would you like to add? Give a number between 1 and {len(query_results)}.\n"))-1
-    if query_results[book_choice] not in reading_list:
-      reading_list.append(query_results[book_choice])
-      print ( f"{query_results[book_choice].title} has been added to you reading list.\n")
-    else:
-      print ( f"{query_results[book_choice].title} is already in your reading list.\n")
-    
-    another_book = None
-    while another_book not in ["yes", "no"]:
-      another_book = input  ("Would you like to add another book? Answer yes or no\n").lower()
-      if another_book not in ["yes", "no"]:
-        print ("that's not a valid option, please try again\n")
-      if another_book == "yes":
-        print_library(query_results)
-      elif another_book == "no":
-        add_book = None
-        
+def add_to_reading_list(query_results, reading_list):
+    """ Ask user if they want to add book a book to reading list. Continue to ask and add until user stops process.
+
+        Parameters:
+            query_results: array (array of books from search, stored as Book objects
+            reading_list: array (from main) (array of books added by user to 'reading list', stored as Book objects)
+        Updates:
+            reading list: array (array of books added by user to 'reading list', stored as Book objects
+    """
+
+    ''' Ask user if they want to add a book to their reading list '''
+    add_book = None
+    while add_book not in ["yes", "no"]:
+        add_book = input("Would you like to add a book to you reading list? Answer yes or no.\n").lower()
+        if add_book not in ["yes", "no"]:
+            print("that's not a valid option, please try again\n")
+
+    ''' Ask user which book they would like to add. Add if not already in list '''
+    while add_book == "yes":
+        book_choice = int(
+            input(f"which book would you like to add? Give a number between 1 and {len(query_results)}.\n")) - 1
+        if query_results[book_choice] not in reading_list:
+            reading_list.append(query_results[book_choice])
+            print(f"{query_results[book_choice].title} has been added to you reading list.\n")
+        else:
+            print(f"{query_results[book_choice].title} is already in your reading list.\n")
+
+        ''' Ask user if they want to add another book, repeat process until user states 'no' '''
+        another_book = None
+        while another_book not in ["yes", "no"]:
+            another_book = input("Would you like to add another book? Answer yes or no\n").lower()
+            if another_book not in ["yes", "no"]:
+                print("that's not a valid option, please try again\n")
+            if another_book == "yes":
+                print_library(query_results)
+            elif another_book == "no":
+                add_book = None
--- google_search.py
+++ google_search.py
@@ -1,62 +1,63 @@
-#Libraries
-import requests
-
-#Files
+#
+import requests  # external library
 from create_book import Book
 from print_library import print_library
 
-#Variables
-
-add_book = None
 
 def do_search():
-  #API key to allow acces to Google Books API
-  api_key = "AIzaSyBFVYm6UHh50nFU9FvbUL6qqnKxfcGMpLQ"
-  
-  #Gather input from user - their search query for Google books
-  user_input = input("what would you like to search Google Books for?\n")
-  
-  #Use user input and API key to create the API request and receive response data
-  api_call = f"https://www.googleapis.com/books/v1/volumes?q={user_input}&key={api_key}"
-  
-  response = requests.get(url = api_call)
-  response.raise_for_status()
-  data = response.json()
+    """ Create Google Books API call using API key and users search request. Present results to user and return
+    query results array containing search results Book objects.
 
-  try:
-      
-    #Create objects of each book, containing key details
-    query_results = []
-    for x in range(min(len(data["items"]),5)):
-      book = Book()
-    
-      #Use try/except to avoid errors where entries are missing title/author/publisher fields
-      try:
-        book.title = data["items"][x]["volumeInfo"]["title"]
-      except:
-        book.title =  "None"
-    
-      try:
-        book.authors = data["items"][x]["volumeInfo"]["authors"]
-      except:
-        book.authors =  "None"
-      
-      try:
-        book.publisher = data["items"][x]["volumeInfo"]["publisher"]
-      except:
-        book.publisher =  "None"
-      
-      book.list_authors()
-      
-      query_results.append(book)
+        Returns:
+            query_results: array (array of books from search, stored as Book objects
+    """
 
-  except:
-    print ("Your search returned no results, check you have entered your query correctly.")
-  
-  #Present user with search restults
-  
-   
-  print ("here are your search results\n")
-  print_library(query_results)
+    ''' API key to allow access to Google Books API '''
+    api_key = "AIzaSyBFVYm6UHh50nFU9FvbUL6qqnKxfcGMpLQ"
 
-  return query_results
+    ''' Gather input from user - their search query for Google books '''
+    user_input = input("what would you like to search Google Books for?\n")
+
+    ''' Use user input and API key to create the API request and receive response data '''
+    api_call = f"https://www.googleapis.com/books/v1/volumes?q={user_input}&key={api_key}"
+
+    response = requests.get(url=api_call)
+    response.raise_for_status()
+    data = response.json()
+
+    try:
+        ''' Create objects of each book, containing key details '''
+        query_results = []
+        for x in range(min(len(data["items"]), 5)):
+            book = Book()
+
+            ''' Use try/except to avoid errors where entries are missing title/author/publisher fields '''
+            try:
+                book.title = data["items"][x]["volumeInfo"]["title"]
+            except KeyError:
+                book.title = "None"
+
+            try:
+                book.authors = data["items"][x]["volumeInfo"]["authors"]
+            except KeyError:
+                book.authors = "None"
+
+            try:
+                book.publisher = data["items"][x]["volumeInfo"]["publisher"]
+            except KeyError:
+                book.publisher = "None"
+
+            ''' convert author arrays to strings (allowing for multiple authors)'''
+            book.list_authors()
+            ''' add '''
+            query_results.append(book)
+
+    except KeyError:
+        print("Your search returned no results, check you have entered your query correctly.")
+
+    ''' Present user with search results '''
+    print("here are your search results\n")
+    print_library(query_results)
+
+    ''' Return query results for use in add_to_reading_list func'''
+    return query_results
